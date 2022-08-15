dabe5e7c1972cbdfa0308c10dcb7ec636c475d37
Matheus Reis <matheus.reis081@gmail.com>
2022-08-15 18:54:48 -0300

    test: ensure that storeLog repository receive correctly params

--- __test__/modules/weather/data/useCases/storeLogUseCase.spec.ts
+++ __test__/modules/weather/data/useCases/storeLogUseCase.spec.ts
@@ -2,8 +2,9 @@ import { EmptyFieldError } from '../../../../../src/global/errors/emptyFieldErro
 import { StoreLogUseCase } from '../../../../../src/modules/weather/data/useCases/storeLogUseCase';
 
 const makeSut = () => {
-  const sut = new StoreLogUseCase();
-  return { sut };
+  const repository = { storeLog: jest.fn() };
+  const sut = new StoreLogUseCase(repository);
+  return { sut, repository };
 };
 
 const params = {
@@ -20,43 +21,59 @@ const params = {
 
 describe('StoreLogUseCase', () => {
   it('should throw a EmptyFieldError if params.city is empty', async () => {
-    const { sut } = makeSut();
+    const { sut, repository } = makeSut();
     const wrongParams = {
       ...params,
       city: ''
     };
     expect(async () => await sut.execute(wrongParams)).rejects.toThrow(new EmptyFieldError('params.city'));
+    expect(repository.storeLog).not.toBeCalled();
   });
   it('should throw a EmptyFieldError if params.actualTemperature is empty', async () => {
-    const { sut } = makeSut();
+    const { sut, repository } = makeSut();
     const wrongParams = {
       ...params,
       actualTemperature: ''
     };
     expect(async () => await sut.execute(wrongParams)).rejects.toThrow(new EmptyFieldError('params.actualTemperature'));
+    expect(repository.storeLog).not.toBeCalled();
   });
   it('should throw a EmptyFieldError if params.minTemperature is empty', async () => {
-    const { sut } = makeSut();
+    const { sut, repository } = makeSut();
     const wrongParams = {
       ...params,
       minTemperature: ''
     };
     expect(async () => await sut.execute(wrongParams)).rejects.toThrow(new EmptyFieldError('params.minTemperature'));
+    expect(repository.storeLog).not.toBeCalled();
   });
   it('should throw a EmptyFieldError if params.maxTemperature is empty', async () => {
-    const { sut } = makeSut();
+    const { sut, repository } = makeSut();
     const wrongParams = {
       ...params,
       maxTemperature: ''
     };
     expect(async () => await sut.execute(wrongParams)).rejects.toThrow(new EmptyFieldError('params.maxTemperature'));
+    expect(repository.storeLog).not.toBeCalled();
   });
   it('should throw a EmptyFieldError if params.cityId is empty', async () => {
-    const { sut } = makeSut();
+    const { sut, repository } = makeSut();
     const wrongParams = {
       ...params,
       cityId: ''
     };
     expect(async () => await sut.execute(wrongParams)).rejects.toThrow(new EmptyFieldError('params.cityId'));
+    expect(repository.storeLog).not.toBeCalled();
+  });
+  it('should storeLog repository method receive correct params', async () => {
+    const { sut, repository } = makeSut();
+
+    const rightParams = {
+      ...params,
+      lastConsult: new Date().getTime()
+    };
+    await sut.execute(params);
+
+    expect(repository.storeLog).toBeCalledWith(rightParams);
   });
 });
--- src/modules/weather/data/useCases/storeLogUseCase.ts
+++ src/modules/weather/data/useCases/storeLogUseCase.ts
@@ -1,14 +1,21 @@
 import { EmptyFieldError } from '../../../../global/errors/emptyFieldError';
 import { Weather } from '../../domain/entities/weather';
 import { iStoreLogUseCase } from '../../domain/iuseCases/istoreLogUseCase';
+import { iStoreLogRepository } from '../irepositories/istoreLogRepository';
 
 export class StoreLogUseCase implements iStoreLogUseCase {
-  execute (params: Weather): Promise<void> {
+  constructor (private repository: iStoreLogRepository) { }
+  async execute (params: Weather): Promise<void> {
     if (!params.city) { throw new EmptyFieldError('params.city'); }
     if (!params.actualTemperature) { throw new EmptyFieldError('params.actualTemperature'); }
     if (!params.minTemperature) { throw new EmptyFieldError('params.minTemperature'); }
     if (!params.maxTemperature) { throw new EmptyFieldError('params.maxTemperature'); }
     if (!params.cityId) { throw new EmptyFieldError('params.cityId'); }
-    throw new Error('Method not implemented.');
+
+    const data = {
+      ...params,
+      lastConsult: new Date().getTime()
+    };
+    await this.repository.storeLog(data);
   }
 }
