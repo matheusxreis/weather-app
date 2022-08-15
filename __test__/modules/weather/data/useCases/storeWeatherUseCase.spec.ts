5a8af5a52b4abbc074d7a81454345febebd8a8a7
Matheus Reis <matheus.reis081@gmail.com>
2022-08-15 17:33:27 -0300

    test: ensure that StoreWeatherUseCase throws if receive empty params

---
+++ __test__/modules/weather/data/useCases/storeWeatherUseCase.spec.ts
@@ -0,0 +1,82 @@
+import { EmptyFieldError } from '../../../../../src/global/errors/emptyFieldError';
+import { StoreWeatherUseCase } from '../../../../../src/modules/weather/data/useCases/storeWeatherUseCase';
+
+const makeSut = () => {
+  const sut = new StoreWeatherUseCase();
+  return { sut };
+};
+
+const params = {
+  cityId: 'SP121212',
+  weather: {
+    city: 'SP',
+    actualTemperature: 'string',
+    minTemperature: 'string',
+    maxTemperature: 'string',
+    iconId: 'string',
+    photo: 'string'
+
+  }
+};
+describe('StoreWeatherUseCase', () => {
+  it('should throws EmptyFieldError if params.city is empty', async () => {
+    const { sut } = makeSut();
+
+    const wrongParams = {
+      city: '',
+      actualTemperature: 'string',
+      minTemperature: 'string',
+      maxTemperature: 'string',
+      iconId: 'string',
+      photo: 'string'
+
+    };
+
+    expect(async () => await sut.execute(wrongParams)).rejects.toThrow(new EmptyFieldError('params.city'));
+  });
+  it('should throws EmptyFieldError if params.actualTemperature is empty', async () => {
+    const { sut } = makeSut();
+
+    const wrongParams = {
+      city: 'SP',
+      actualTemperature: '',
+      minTemperature: 'string',
+      maxTemperature: 'string',
+      iconId: 'string',
+      photo: 'string'
+
+    };
+
+    expect(async () => await sut.execute(wrongParams)).rejects.toThrow(new EmptyFieldError('params.actualTemperature'));
+  });
+  it('should throws EmptyFieldError if params.minTemperature is empty', async () => {
+    const { sut } = makeSut();
+
+    const wrongParams = {
+      city: 'SP',
+      actualTemperature: 'string',
+      minTemperature: '',
+      maxTemperature: 'string',
+      iconId: 'string',
+      photo: 'string'
+
+    };
+
+    expect(async () => await sut.execute(wrongParams)).rejects.toThrow(new EmptyFieldError('params.minTemperature'));
+  });
+  it('should throws EmptyFieldError if params.maxTemperature is empty', async () => {
+    const { sut } = makeSut();
+
+    const wrongParams = {
+      city: 'SP',
+      actualTemperature: 'string',
+      minTemperature: 'string',
+      maxTemperature: '',
+      iconId: 'string',
+      photo: 'string'
+
+    };
+
+    expect(async () => await sut.execute(wrongParams)).rejects.toThrow(new EmptyFieldError('params.maxTemperature'));
+  });
+});
