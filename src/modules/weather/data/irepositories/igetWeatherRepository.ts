e579bcff1348264963b5a3976259e03270200572
Matheus Reis <matheus.reis081@gmail.com>
2022-08-15 16:45:44 -0300

    feat: created iGetWeatherRepository

---
+++ src/modules/weather/data/irepositories/igetWeatherRepository.ts
@@ -0,0 +1,20 @@
+type WeatherInfoParams = {
+    latitute:string,
+    longitude:string
+}
+
+export interface GetWeatherResponse {
+   main: {
+    temp: number,
+    feels_like: number,
+    temp_min: number,
+    temp_max: number,
+    pressure: number,
+    humidity: number
+   },
+   id: number,
+   name: string
+}
+export interface iGetWeatherRepository {
+getWeather(params: WeatherInfoParams):Promise<GetWeatherResponse>
+}
