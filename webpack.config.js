fc1d392cec4dc1613fde66c83e4b9b84ce28294b
Matheus Reis <matheus.reis081@gmail.com>
2022-08-15 21:32:38 -0300

    chore: added ts-loader to resolver ts files in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
-const isDev = process.env.NODE_ENV === "development"; 
-
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-    mode: isDev ? "development" : "production",
-    devtool: isDev ? "eval-source-map" : "source-map",
-    entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-    },
-    devServer: {
-        static: path.resolve(__dirname, 'public'),
-        hot: true
-    },
-    plugins: [
-        isDev && new ReactRefreshWebpackPlugin(), 
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        })
-    ].filter(Boolean),
-    module: {
-        rules: [
-            {
-                test: /\.(j|t)sx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader:'babel-loader',
-                    options: {
-                        plugins: [
-                            isDev && require.resolve('react-refresh/babel')
-                        ].filter(Boolean)
-                    }
-                }
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader','sass-loader']
-            }
-        ]
-    }
-}
+  mode: isDev ? 'development' : 'production',
+  devtool: isDev ? 'eval-source-map' : 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+    hot: true
+  },
+  plugins: [
+    isDev && new ReactRefreshWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDev && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      { test: /\.ts$/, use: 'ts-loader' }
+    ]
+  }
+};
