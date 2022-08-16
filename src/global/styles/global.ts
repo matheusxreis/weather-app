5269a5b7466b9fe2e0e0f1e29c992898e78c3c06
Matheus Reis <matheus.reis081@gmail.com>
2022-08-16 12:07:29 -0300

    refactor: changing colors of theme

--- src/global/styles/global.ts
+++ src/global/styles/global.ts
@@ -3,8 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
  body {
 
-  background: #fafafa;
- }; 
+  background: ${({ theme }) => theme.colors.backgroundSecondary};
+ };
  * {
    padding: 0px;
    margin: 0px;
