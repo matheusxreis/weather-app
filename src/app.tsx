936daa4fa1d9c46924b2bc2a28a6bab96f37dc13
Matheus Reis <matheus.reis081@gmail.com>
2022-08-15 21:04:02 -0300

    feat: presentation layer created

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,7 @@ import { GlobalStyle } from './global/styles/global';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './global/store/index';
+import { Home } from './modules/weather/presentation/Home';
 
 export default function App () {
   const [counter, setCounter] = useState<number>(0);
@@ -16,10 +17,7 @@ export default function App () {
     <ReduxProvider store={store}>
     <PersistGate loading={null} persistor={persistor}>
     <ThemeProvider theme={theme}>
-        <h1> Hello World 2e3 </h1>
-        <h3> {counter} </h3>
-        <button onClick={() => incrementCounter()}> increment </button>
-        <button onClick={() => decrementCounter()}> decrement </button>
+      <Home />
     <GlobalStyle />
 
     </ThemeProvider>
---
+++ src/modules/weather/presentation/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as Component from './styles';
+
+export function Home () {
+  return (
+        <Component.Container>
+            
+        </Component.Container>
+  );
+}
