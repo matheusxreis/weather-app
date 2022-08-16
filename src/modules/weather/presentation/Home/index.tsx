464295ee1419561667a5ba793d9905bc6c5de7e4
Matheus Reis <matheus.reis081@gmail.com>
2022-08-16 18:16:07 -0300

    feat: data uptading if change in firebase console

--- src/modules/weather/presentation/Home/index.tsx
+++ src/modules/weather/presentation/Home/index.tsx
@@ -9,18 +9,22 @@ import { StoreWeatherUseCase } from '../../data/useCases/storeWeatherUseCase';
 import { StoreLogUseCase } from '../../data/useCases/storeLogUseCase';
 import { useDispatch } from 'react-redux';
 import { updateWeather } from '../../../../global/store/weather/actions';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { db } from '../../infra/helpers/firebaseConfig';
+import { collection } from 'firebase/firestore';
 
 type CityType = {
 name:string,
 lat:string,
-lon:string
+lon:string,
+id:string
 }
 const cities: CityType[] = [
-  { name: 'São Paulo', lon: '-49.0', lat: '-22.0' },
-  { name: 'Nova York', lon: '-74.005966', lat: '40.714272' },
-  { name: 'Tokyo', lon: '139.691711', lat: '35.689499' },
-  { name: 'Paris', lon: '2.3486', lat: '48.853401' },
-  { name: 'Amsterdam', lon: '4.88969', lat: '52.374031' }
+  { name: 'São Paulo', lon: '-49.0', lat: '-22.0', id: '3448433' },
+  { name: 'Nova York', lon: '-74.005966', lat: '40.714272', id: '5128581' },
+  { name: 'Tokyo', lon: '139.691711', lat: '35.689499', id: '1850144' },
+  { name: 'Paris', lon: '2.3486', lat: '48.853401', id: '2988507' },
+  { name: 'Amsterdam', lon: '4.88969', lat: '52.374031', id: '2759794' }
 
 ];
 const getWeather = () => {
@@ -45,15 +49,15 @@ export function Home () {
   const [weather, setWeather] = useState<Weather>({} as Weather);
   const [actualCity, setActualCity] = useState<CityType>(cities[0]);
   const dispatch = useDispatch();
+  const [consultValues] = useCollection(
+    collection(db, 'consult')
+  );
 
   async function getWeatherInfo (lat:string, lon:string) {
     try {
-      console.log('ooooi222');
-
       const response = await getWeather().execute({ latitute: lat, longitude: lon }).then(x => x);
-      setWeather(response);
       dispatch(updateWeather(response));
-      // dispatch(updateMinWeather(response));
+      if (!consultValues?.docs.length) { setWeather(response); }
       await saveWeather().execute(response);
       await saveLogs().execute(response);
     } catch (err) {
@@ -62,18 +66,23 @@ export function Home () {
   }
 
   async function changeCityWeather (city:CityType) {
-    console.log('ooooi');
     setActualCity(city);
     await getWeatherInfo(city.lat, city.lon);
   }
   function isCitySelected (city: CityType) {
     return city.name === actualCity.name;
   }
-
   useEffect(() => {
     getWeatherInfo(actualCity.lat, actualCity.lon);
   }, []);
-  useEffect(() => { console.log(weather); }, [weather]);
+
+  useEffect(() => {
+    consultValues?.docs.forEach((x:any) => {
+      if (x.data().cityId === actualCity.id) {
+        setWeather(x.data() as Weather);
+      }
+    });
+  }, [consultValues, actualCity]);
 
   return (
         <Component.Container>
@@ -92,13 +101,16 @@ export function Home () {
             ))}
 
             </Component.SelectWeatherButtonContainer>
+            {weather && (
+
             <WeatherView
             actualTemperature={weather?.actualTemperature}
             city={weather?.city}
             image=''
             maxTemperature={weather?.maxTemperature}
             minTemparature={weather?.minTemperature}
-        />
+           />
+            )}
             </Component.Card>
 
         </Component.Container>
