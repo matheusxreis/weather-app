558989ae78d5f01f79ccdfdc40087a81448e5cf0
Matheus Reis <matheus.reis081@gmail.com>
2022-08-16 00:15:51 -0300

    feat: base of WeatherView component done

---
+++ src/modules/weather/presentation/WeatherView/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import * as Component from './styles';
+import { BsFillSunriseFill as MaxIcon, BsFillSunsetFill as MinIcon, BsThermometerSun as ActualIcon } from 'react-icons/bs';
+
+type WeatherViewProps = {
+    actualTemperature: string,
+    minTemparature:string,
+    maxTemperature:string,
+    image: string,
+    city:string
+}
+
+export function WeatherView ({
+  actualTemperature,
+  minTemparature,
+  maxTemperature,
+  image,
+  city
+}: WeatherViewProps) {
+  function isSun () {
+    if (Number(actualTemperature) > 24) { return true; }
+    return false;
+  }
+  function getTextLabel () {
+    if (Number(actualTemperature) > 24) { return 'Wow! Acho que dá pra aproveitar!'; }
+    return 'Que clima, hein !?';
+  }
+
+  return (
+        <Component.Container>
+            <Component.DataWeatherContainer>
+            <Component.TimeNowIn>Tempo agora em: {city}. </Component.TimeNowIn>
+            <Component.Pharagraph><ActualIcon size={24}/>  Temperatura atual: {actualTemperature} °C.</Component.Pharagraph>
+            <Component.Pharagraph><MaxIcon size={24} />  Temperatura mínima: {minTemparature} °C. </Component.Pharagraph>
+            <Component.Pharagraph><MinIcon size={24}/>  Temperatura máxima: {maxTemperature} °C. </Component.Pharagraph>
+            <Component.TemperatureLabel isSun={isSun()}>
+                    <Component.Pharagraph> {getTextLabel()} </Component.Pharagraph>
+            </Component.TemperatureLabel>
+            </Component.DataWeatherContainer>
+
+            <Component.Image src={'https://georgiatoday.ge/wp-content/uploads/2021/07/rain-umbrella-vancouver-weather.jpg'} />
+
+        </Component.Container>
+  );
+}
---
+++ src/modules/weather/presentation/WeatherView/styles.tsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export type LabelProps = {
+    isSun:boolean;
+}
+export const Container = styled.div`
+
+    background: #fafafa;
+    filter: brightness(0.95);
+    border-radius:7px;
+    padding:14px 54px;
+    height:200px;
+    display:flex;
+    align-items:center;
+    justify-content: space-between;
+    
+`;
+
+export const TimeNowIn = styled.h2`
+    
+font-size:26px;
+font-family: ${({ theme }) => theme.fonts.normal};
+color: ${({ theme }) => theme.colors.textSecondary};
+
+`;
+
+export const Pharagraph = styled.p`
+    
+font-size:20px;
+display:flex;
+text-align:center;
+font-family: ${({ theme }) => theme.fonts.normal};
+color: ${({ theme }) => theme.colors.textSecondary};
+margin: 5px 0px;
+
+`;
+
+export const DataWeatherContainer = styled.div``;
+export const Image = styled.img`
+    
+    width:200px;
+    height:200px;
+    border-radius:200px;
+`;
+
+export const TemperatureLabel = styled.div<LabelProps>`
+    background: ${({ theme }) => theme.colors.backgroundPrimary};
+    width:100%;
+    border-left: ${({ theme, isSun }) => isSun ? `2px solid ${theme.colors.secondary}` : `2px solid ${theme.colors.primary}`};
+    padding-left:10px;
+    height:30px;
+    border-radius:5px;
+    margin:17px 0px;
+
+`;
