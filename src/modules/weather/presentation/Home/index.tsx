1175fef8256659c1b4bc37afc8d65b1a236a98bf
Matheus Reis <matheus.reis081@gmail.com>
2022-08-16 00:15:23 -0300

    feat: base of Home page done

--- src/modules/weather/presentation/Home/index.tsx
+++ src/modules/weather/presentation/Home/index.tsx
@@ -2,10 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { GetWeatherInfoUseCase } from '../../data/useCases/getWeatherInfoUseCase';
 import { Weather } from '../../domain/entities/weather';
 import { AxiosRepository } from '../../infra/axiosRepository';
+import { WeatherView } from '../WeatherView';
 import * as Component from './styles';
+import { BsCloudSunFill as WeatherIcon } from 'react-icons/bs';
+import { useTheme } from 'styled-components';
 
+type CityType = {
+name:string,
+lat:string,
+lon:string
+}
 export function Home () {
   const [weather, setWeather] = useState<Weather>({} as Weather);
+  const [actualCity, setActualCity] = useState<CityType>({ name: 'São Paulo', lon: '1122', lat: '11212' });
+
+  const cities: CityType[] = [
+    { name: 'São Paulo', lon: '1122', lat: '11212' },
+    { name: 'Nova York', lon: '1122', lat: '11212' },
+    { name: 'Tokyo', lon: '1122', lat: '11212' },
+    { name: 'Londres', lon: '1122', lat: '11212' },
+    { name: 'Amsterdam', lon: '1122', lat: '11212' }
+
+  ];
+
   const getWeather = () => {
     const r = new AxiosRepository();
     const getWeatherUseCase = new GetWeatherInfoUseCase(r);
@@ -18,17 +37,51 @@ export function Home () {
     } catch {}
   }
 
+  function changeCityWeather (city:CityType) {
+    setActualCity(city);
+  }
+  function isCitySelected (city: CityType) {
+    return city.name === actualCity.name;
+  }
+
   useEffect(() => {
     getWeatherInfo();
   }, []);
 
   return (
         <Component.Container>
-            <h1> Clima </h1>
-            <p> </p>
-            <p> </p>
-            <p> </p>
-            <p> </p>
+            <Component.Header>
+                <Component.TitleContainer>
+                <Component.Title> Weather App </Component.Title>
+                <Component.SubTitle> Veja o clima pelo mundo. </Component.SubTitle>
+                </Component.TitleContainer>
+            </Component.Header>
+            <Component.Card>
+            <Component.SelectWeatherButtonContainer>
+
+            {cities.map(x => (
+            <Component.SelectWeatherButton
+            onClick={() => changeCityWeather(x)}
+            isSelected={isCitySelected(x)}
+            key={x.name}>
+                {x.name}
+            </Component.SelectWeatherButton>
+
+            ))}
+             <WeatherIcon
+                size={30}
+                color={useTheme().colors.textSecondary} />
+
+            </Component.SelectWeatherButtonContainer>
+            <WeatherView
+            actualTemperature='25'
+            city='São Paulo'
+            image=''
+            maxTemperature='29'
+            minTemparature='19'
+        />
+            </Component.Card>
+
         </Component.Container>
   );
 }
--- src/modules/weather/presentation/Home/styles.ts
+++ src/modules/weather/presentation/Home/styles.ts
@@ -1,3 +1,85 @@
 import styled from 'styled-components';
 
+type ButtonProps = {
+    isSelected:boolean;
+}
+
 export const Container = styled.div``;
+
+export const Title = styled.h1`
+    font-family: ${({ theme }) => theme.fonts.title};
+    font-size: 60px;
+    color: ${({ theme }) => theme.colors.textPrimary};
+
+
+`;
+
+export const SubTitle = styled.h1`
+font-family: ${({ theme }) => theme.fonts.normal};
+    font-size: 26px;
+    color: ${({ theme }) => theme.colors.textPrimary};
+    font-weight: 200;
+`;
+export const SelectWeatherButton = styled.button<ButtonProps>`
+
+background: ${({ isSelected }) => isSelected ? '#fafafa' : 'transparent'};;
+color: ${({ theme, isSelected }) => theme.colors.textSecondary};
+filter:${({ isSelected }) => isSelected ? 'brightness(0.95)' : 'brightness(1)'};
+font-size: 22px;
+font-weight:150px;
+width: 150px;
+height: 40px;
+border: 0px;
+border-radius:2px;
+margin-top:10px;
+font-family: ${({ theme }) => theme.fonts.normal};
+font-size:16px;
+
+
+transition: 0.5s;
+cursor: pointer;
+&:hover {
+    filter: brightness(0.95);
+    background: #fafafa;
+}
+
+
+`;
+
+export const Header = styled.div`
+    //background: ${({ theme }) => theme.colors.backgroundSecondary};
+    background-image:url('https://centralca.cdn-anvilcms.net/media/images/2019/01/02/images/Rainy_Weather_pix.max-1200x675.jpg');
+    width:100%;
+    flex-direction:column;
+    padding: 0px 0px;
+   
+`;
+export const TitleContainer = styled.div`
+     margin:0 auto;
+     display: flex;
+     flex-direction:column;
+     align-items:center;
+     backdrop-filter: blur(60px);
+     padding:30px;
+     border-radius:7px;
+`;
+export const SelectWeatherButtonContainer = styled.div`
+    display:flex;
+    align-items:center;
+    flex-wrap:wrap;
+    justify-content:space-around;
+    width:800px;
+    margin: 0 auto;
+    max-width:100%;
+
+`;
+export const Card = styled.div`
+
+    width:1000px;
+    margin: 0 auto;
+    max-width:100%;
+    background: ${({ theme }) => theme.colors.backgroundPrimary};
+    border-radius:2px;
+
+
+`;
