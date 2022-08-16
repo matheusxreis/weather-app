887e636b68fc7f2a0910358a63331b31fa24a308
Matheus Reis <matheus.reis081@gmail.com>
2022-08-16 11:27:17 -0300

    chore: added output.publicPath and devServer.historyApiFallback in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,17 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
   devServer: {
     static: path.resolve(__dirname, 'public'),
-    hot: true
+    hot: true,
+    historyApiFallback: true
+
   },
   plugins: [
     isDev && new ReactRefreshWebpackPlugin(),
