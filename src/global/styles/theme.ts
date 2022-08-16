6c0af675282ddb06b6c33189b4d9771e185dd748
Matheus Reis <matheus.reis081@gmail.com>
2022-08-16 00:14:29 -0300

    feat: update in colors of theme

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,6 +1,14 @@
 export const theme = {
   colors: {
-    primary: '#212930',
-    textPrimary: '#fff'
+    primary: '#095879',
+    secondary: '#CC4743',
+    textPrimary: '#fff',
+    backgroundPrimary: '#fff',
+    backgroundSecondary: '#0E0F0F',
+    textSecondary: '#0E0F0F'
+  },
+  fonts: {
+    title: '"Great Vibes", cursive;',
+    normal: '"Roboto", sans-serif;'
   }
 };
