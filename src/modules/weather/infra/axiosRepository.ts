468581fcfbb6a84aef17c45a5a4c08904f92678a
Matheus Reis <matheus.reis081@gmail.com>
2022-08-17 08:39:56 -0300

    refactor: added api key as env variable

--- src/modules/weather/infra/axiosRepository.ts
+++ src/modules/weather/infra/axiosRepository.ts
@@ -6,7 +6,7 @@ class AxiosRepository implements iGetWeatherRepository {
   async getWeather (params: { latitute: string; longitude: string; }): Promise<GetWeatherResponse> {
     if (!params.latitute) { throw new EmptyFieldError('params.latitude'); }
     if (!params.longitude) { throw new EmptyFieldError('params.longitude'); }
-    const response = await api.get(`weather?lat=${params.latitute}&lon=${params.longitude}&units=metric&&appid=${'ca901cb68cac0e43b462bb91626bee72'}`);
+    const response = await api.get(`weather?lat=${params.latitute}&lon=${params.longitude}&units=metric&&appid=${process.env.API_KEY}`);
     ;
     return response.data;
   }
