b509e21e7a922560dda1723c5cb0dfa0eda7ddf9
Matheus Reis <matheus.reis081@gmail.com>
2022-08-17 09:39:14 -0300

    test: ensure that firebase method receive right params

--- __test__/modules/weather/infra/firebaseRepository.spec.ts
+++ __test__/modules/weather/infra/firebaseRepository.spec.ts
@@ -1,7 +1,7 @@
 import { FirebaseRepository } from '../../../../src/modules/weather/infra/firebaseRepository';
 import { EmptyFieldError } from '../../../../src/global/errors/emptyFieldError';
 
-import firestore, { collection } from 'firebase/firestore';
+import firestore from 'firebase/firestore';
 
 const makeSut = () => {
   const sut = new FirebaseRepository();
@@ -14,15 +14,16 @@ const params = {
   minTemperature: 'min_temperature',
   maxTemperature: 'max_temperature',
   lastConsult: new Date().getTime(),
-  cityId: 'city_id'
+  cityId: 'city_id',
+  photo: ''
 
 };
 
 jest.mock('firebase/firestore', () => {
   return {
     addDoc: jest.fn(),
-    collection: jest.fn(),
-    getFirestore: jest.fn()
+    collection: jest.fn().mockImplementation(() => 'fakeCollection'),
+    getFirestore: jest.fn().mockImplementation(() => 'fakeDB')
   };
 });
 
@@ -101,5 +102,19 @@ describe('FirebaseRepository', () => {
     expect(async () => await sut.storeLog(wrongParams))
       .rejects
       .toThrow(new EmptyFieldError('params.cityId'));
+  });
+  it('should collection method be receive correct params in storeLog method', async () => {
+    const { sut } = makeSut();
+
+    await sut.storeLog(params);
+
+    expect(firestore.collection).toBeCalledWith('fakeDB', 'log');
+  });
+  it('should addDB method be receive correct params git in storeLog method', async () => {
+    const { sut } = makeSut();
+
+    await sut.storeLog(params);
+
+    expect(firestore.addDoc).toBeCalledWith('fakeCollection', params);
   });
 });
