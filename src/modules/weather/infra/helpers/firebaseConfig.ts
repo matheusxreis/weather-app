9ef84fc6fd8dc78670652a98a9734aa217e6bd38
Matheus Reis <matheus.reis081@gmail.com>
2022-08-17 14:00:40 -0300

    chore: update in firebase config

--- src/modules/weather/infra/helpers/firebaseConfig.ts
+++ src/modules/weather/infra/helpers/firebaseConfig.ts
@@ -9,7 +9,6 @@ import { getFirestore } from 'firebase/firestore';
 const app = initializeApp({
   apiKey: process.env.FB_API_KEY,
   authDomain: process.env.FB_AUTH_DOMAIN,
-  databaseURL: process.env.FB_DATABASE_URL,
   projectId: process.env.FB_PROJECT_ID,
   storageBucket: process.env.FB_STORAGE_BUCKET,
   messagingSenderId: process.env.FB_MESSAGING_SEND_ID,
