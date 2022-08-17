54b43d7ee6ad0e6f9e3ec8073b052cc392a3fcfa
Matheus Reis <matheus.reis081@gmail.com>
2022-08-17 10:20:11 -0300

    refactor: update of according with tests in firebaseRepository

--- src/modules/weather/infra/firebaseRepository.ts
+++ src/modules/weather/infra/firebaseRepository.ts
@@ -2,16 +2,19 @@ import { iStoreLogRepository } from '../data/irepositories/istoreLogRepository';
 import { iStoreWeatherRepository, WeatherStoreParams } from '../data/irepositories/iStoreWeatherRepository';
 import { db } from './helpers/firebaseConfig';
 import { doc, collection, addDoc, where, query, getDocs, updateDoc } from 'firebase/firestore';
+import { EmptyFieldError } from '../../../global/errors/emptyFieldError';
 
 export class FirebaseRepository implements iStoreLogRepository, iStoreWeatherRepository {
   async storeLog (params: WeatherStoreParams): Promise<void> {
+    this.verifyParams(params);
+
     const data = {
       actualTemperature: params.actualTemperature,
       minTemperature: params.minTemperature,
       maxTemperature: params.maxTemperature,
       city: params.city,
       cityId: params.cityId,
-      photo: params.photo ? params.photo : null,
+      photo: params.photo ? params.photo : '',
       lastConsult: params.lastConsult
     };
     const col = collection(db, 'log');
@@ -19,13 +22,15 @@ export class FirebaseRepository implements iStoreLogRepository, iStoreWeatherRep
   }
 
   async storeWeather (params: WeatherStoreParams): Promise<void> {
+    this.verifyParams(params);
+
     const data = {
       actualTemperature: params.actualTemperature,
       minTemperature: params.minTemperature,
       maxTemperature: params.maxTemperature,
       city: params.city,
       cityId: params.cityId,
-      photo: params.photo ? params.photo : null,
+      photo: params.photo ? params.photo : '',
       lastConsult: params.lastConsult
     };
     const col = collection(db, 'consult');
@@ -33,23 +38,24 @@ export class FirebaseRepository implements iStoreLogRepository, iStoreWeatherRep
   }
 
   async findConsultById (cityId: string): Promise<WeatherStoreParams | null> {
+    if (!cityId) { throw new EmptyFieldError('cityId'); }
     const q = query(collection(db, 'consult'), where('cityId', '==', cityId));
     const querySnapshot = await getDocs(q);
     let city;
 
     querySnapshot.forEach((doc) => {
-      console.log(doc.data(), 'data');
       city = doc.data();
     });
     return city as unknown as WeatherStoreParams;
   }
 
   async updateConsult (params: WeatherStoreParams): Promise<void> {
+    this.verifyParams(params);
+
     const q = query(collection(db, 'consult'), where('cityId', '==', params.cityId));
     const querySnapshot = await getDocs(q);
     let id = '';
     querySnapshot.forEach((doc) => {
-      console.log(doc.data(), 'data');
       id = doc.id;
     });
 
@@ -61,9 +67,17 @@ export class FirebaseRepository implements iStoreLogRepository, iStoreWeatherRep
       maxTemperature: params.maxTemperature,
       city: params.city,
       cityId: params.cityId,
-      photo: params.photo ? params.photo : null,
+      photo: params.photo ? params.photo : '',
       lastConsult: params.lastConsult
     };
     await updateDoc(docRef, data);
+  }
+
+  verifyParams (params: WeatherStoreParams) {
+    if (!params.city) { throw new EmptyFieldError('params.city'); }
+    if (!params.maxTemperature) { throw new EmptyFieldError('params.maxTemperature'); }
+    if (!params.minTemperature) { throw new EmptyFieldError('params.minTemperature'); }
+    if (!params.actualTemperature) { throw new EmptyFieldError('params.actualTemperature'); }
+    if (!params.cityId) { throw new EmptyFieldError('params.cityId'); }
   }
 }
