bf64fc6b9be682066280cdf6ec5186f577b4227b
Matheus Reis <matheus.reis081@gmail.com>
2022-08-17 12:24:07 -0300

    feat: defined background image

--- src/global/styles/global.ts
+++ src/global/styles/global.ts
@@ -1,14 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
-
+import BackgroundImage from '../assets/jpg/background-image.jpg';
 export const GlobalStyle = createGlobalStyle`
  body {
 
   //background: ${({ theme }) => theme.colors.backgroundSecondary};
-  background-image: url('https://img.wallpapersafari.com/desktop/1920/1080/6/79/vbFBVl.jpg');
-  background-position:center;
- };
- * {
-   padding: 0px;
-   margin: 0px;
- }
+    background-image: url('${BackgroundImage}');
+    background-position:end;
+
+*{  margin: 0px;
+    padding: 0px;
+}
 `;
